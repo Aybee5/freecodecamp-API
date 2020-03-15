b186166ba7aac1fec51549b4f4b0c3926f7c495c
Glitch (sticky-band-bottom) <none>
2020-03-15 11:28:15 +0000

    👨‍❤️‍💋‍👨✋ Updated with Glitch

--- myApp.js
+++ myApp.js
@@ -2,11 +2,15 @@
 var express = require('express');
 var app = express();
 
+let bodyParser = require('body-parser')
 // --> 7)  Mount the Logger middleware here
-
+app.use((req, res, next)=>{
+  console.log(req.method + " " + req.path + " - " + req.ip)
+  next()
+})
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({extended: false}))
 
 /** 1) Meet the node console. */
 
@@ -20,37 +24,64 @@ console.log("Hello World")
 // })
 
 /** 3) Serve an HTML file */
-
+app.get('/', (req,res)=>{
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 /** 4) Serve static assets  */
-
+app.use( express.static(__dirname + '/public'))
 
 /** 5) serve JSON on a specific route */
-
+// app.get('/json', (req, res)=> {
+//   res.json({"message": "Hello json"})
+// })
 
 /** 6) Use the .env file to configure the app */
- 
+app.get('/json', (req, res)=>{
+  if(process.env.MESSAGE_STYLE === 'uppercase'){
+    res.json({"message": "Hello json".toUpperCase()})
+  }
+  else{
+    res.json({"message": "Hello json"})
+  }
+})
  
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', (req, res, next)=>{
+  req.time = new Date().toString()
+  next()
+}, (req,res)=>{
+  res.send({time:req.time})
+})
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo',(req, res, next) => {
+  let word = req.params.word
+  res.json({"echo": word})
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get('/name', (req,res)=>{
+  let firstname = req.query.first
+  let lastname = req.query.last
+  res.json({"name": `${firstname} ${lastname}` })
+})
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
 
 /** 12) Get data form POST  */
-
+app.post('/name', (req, res)=>{
+  let firstname = req.body.first
+  let lastname = req.body.last
+  res.json({"name": `${firstname} ${lastname}` })
+})
 
 
 // This would be part of the basic setup of an Express app
